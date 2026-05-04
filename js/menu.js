aa08f16e9c70c412d6a49e35be4af50a77864fd2
fi4klif <fi4klif@gmail.com>
2026-05-04 20:00:13 +0300

    fix btn-pag, fix header, fix mob-menu, fix address

--- js/menu.js
+++ js/menu.js
@@ -2,21 +2,22 @@
   const menuBtnRef = document.querySelector(".menu-btn-open");
   const closeBtnRef = document.querySelector(".menu-close-btn");
   const mobileMenuRef = document.querySelector(".mob-menu");
-
-  // Додаємо пошук усіх посилань у мобільному меню
-  const menuLinks = document.querySelectorAll(".mob-link");
+  const bodyRef = document.body;
 
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
+    bodyRef.classList.toggle("no-scroll");
   });
 
   closeBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.remove("is-open");
+    bodyRef.classList.remove("no-scroll");
   });
-
+  const menuLinks = document.querySelectorAll(".mob-link");
   menuLinks.forEach((link) => {
     link.addEventListener("click", () => {
       mobileMenuRef.classList.remove("is-open");
+      bodyRef.classList.remove("no-scroll");
     });
   });
 })();
